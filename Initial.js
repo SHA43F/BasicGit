af850884353febf48da448d8537e5ea4f0fa24e1
SHA43F <shareefpmk44@gmail.com>
2022-09-19 21:47:04 +0530

    Get element by class name

--- Initial.js
+++ Initial.js
@@ -9,3 +9,12 @@ var itemm=document.getElementById('items-m');
 head.style.border='solid 3px #000';
 itemm.style.color="green"
 itemm.style.fontWeight="bold"
+
+// Get Element By Class
+var lists = document.getElementsByClassName('list-group-item')
+console.log(lists)
+lists[2].style.backgroundColor = 'green'
+for(let i=0;i<lists.length;i++)
+{
+    lists[i].style.fontWeight ="bold"
+}
