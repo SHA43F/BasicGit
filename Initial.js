7fbb0e9e1498eabc6b67da657f90922b71e7ddfe
SHA43F <shareefpmk44@gmail.com>
2022-09-19 17:52:05 +0530

    Lets get started with DOM

--- Initial.js
+++ Initial.js
@@ -1,1 +1,11 @@
-console.log('Print this values.')
+console.log(document.URL)
+console.log(document.images)
+console.log(document.title="Modified by JS")
+
+// Get Element By Id
+var head=document.getElementById('main-header');
+var itemm=document.getElementById('items-m');
+
+head.style.border='solid 3px #000';
+itemm.style.color="green"
+itemm.style.fontWeight="bold"
