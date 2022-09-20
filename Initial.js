41b5ac66ff5e3fad81813a6b9d21fe46dddd8a48
SHA43F <shareefpmk44@gmail.com>
2022-09-20 12:46:56 +0530

    Creating nodes

--- Initial.js
+++ Initial.js
@@ -38,13 +38,49 @@
 // secList.style.backgroundColor = 'green'
 // triList.style.color = 'transparent'
 
-//Query Selector All
+// //Query Selector All
 
-let secListAll = document.querySelectorAll('.list-group-item');
-let oddListAll = document.querySelectorAll('.list-group-item:nth-child(odd)');
+// let secListAll = document.querySelectorAll('.list-group-item');
+// let oddListAll = document.querySelectorAll('.list-group-item:nth-child(odd)');
 
-secListAll[1].style.color = 'green';
-for(let i=0;i<oddListAll.length;i++)
-{
-    oddListAll[i].style.backgroundColor = 'green'
-}
+// secListAll[1].style.color = 'green';
+// for(let i=0;i<oddListAll.length;i++)
+// {
+//     oddListAll[i].style.backgroundColor = 'green'
+// }
+
+// Traversing the DOM
+let trav = document.querySelector('#items')
+console.log(trav.parentElement)
+
+console.log(trav.lastChild)
+console.log(trav.lastElementChild.style.color = 'red')
+
+console.log(trav.firstChild)
+console.log(trav.firstElementChild.style.color = 'blue')
+
+console.log(trav.nextSibling)
+console.log(trav.nextElementSibling)
+
+console.log(trav.previousSibling)
+console.log(trav.previousElementSibling.style.backgroundColor = 'green')
+
+//Creating a new Element
+let elem = document.createElement('div');
+
+elem.className = 'heads'
+elem.id = 'hhdd'
+elem.setAttribute('title', 'hello-title');
+
+let elemVal = document.createTextNode('HELLO')
+
+elem.appendChild(elemVal);
+
+let headNode = document.querySelector('header .container');
+let h1 = document.querySelector('header h1')
+headNode.insertBefore(elem, h1)
+
+let hells = document.createElement('div')
+hells.className = 'he'
+console.log(hells)
+
