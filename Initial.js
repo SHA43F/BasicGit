ea0f9a5814ba2d5a3fca7aa1388de8148ee46329
SHA43F <shareefpmk44@gmail.com>
2022-09-21 12:35:34 +0530

    Filter

--- Initial.js
+++ Initial.js
@@ -1,16 +1,19 @@
 let itemVal = document.getElementById('addForm')
 let items = document.getElementById('items')
+let filt = document.getElementById('filter')
 
 itemVal.addEventListener('submit', additem)
 items.addEventListener('click', delVal)
+filt.addEventListener('keyup', filtval)
 
 function additem(e)
 {
     e.preventDefault();
     let inputStr = document.getElementById('item').value
+    let inputDesc = document.getElementById('descript').value
     let newItem = document.createElement('li')
     newItem.className = 'list-group-item'
-    newItem.appendChild(document.createTextNode(inputStr));
+    newItem.appendChild(document.createTextNode(inputStr+ ' '+inputDesc));
     items.appendChild(newItem)
     let deleteBtn = document.createElement('button')
     deleteBtn.className = "btn btn-danger btn-sm float-right delete"
@@ -35,4 +38,21 @@ function delVal(e)
         items.removeChild(newItem);
     }
     
+}
+
+function filtval(e)
+{
+    let txt = e.target.value.toLowerCase();
+    let itemtxt = document.getElementsByTagName('li')
+    Array.from(itemtxt).forEach(function(item)
+    {
+        let itemString =item.firstChild.textContent;
+        if(itemString.toLowerCase().indexOf(txt)!=-1)
+        {
+            item.style.display = 'block';
+        }
+        else{
+            item.style.display = 'none'
+        }
+    });
 }
