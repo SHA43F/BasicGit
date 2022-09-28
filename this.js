d6fe5c4173c29b60d4e657bfc8017b9401195e44
SHA43F <shareefpmk44@gmail.com>
2022-09-28 16:15:51 +0530

    fat arrow

--- this.js
+++ this.js
@@ -1,27 +1,43 @@
+// 1)
+// Fat arrow function
+
+// let a = function(){
+//     return 1
+// }
+let a = () => 1
+console.log(a())
+
+var add = (b,c) =>{
+    return b*c
+}
+console.log(add(2,3))
+
+var add =(...n) =>{
+    return n
+}
+console.log(add(1,2,3,4,5))
+
+// 2)
+// 3)
 class Student
 {
-    static count=0
     constructor(name,age,phNumber,marks)
     {
         this.name = name;
         this.age = age;
         this.phNumber = phNumber;
         this.marks = marks;
-        Student.count++
     }
-    elig(m) {
-        if(m>=40)
-        {
-            console.log(`${this.name} is eligible.`)
+    elig() {
+        let eligiblity = (age) =>{
+            const minAge = 22;
+            const minMarks = 40;
+            if(age>=minAge && this.marks>=minMarks)
+            {
+                console.log(this.name)
+            }
         }
-        else
-        {
-            console.log(`${this.name} is not eligible.`) 
-        }
-    }
-    static studentCount()
-    {
-        console.log("Students Count: "+Student.count)
+        eligiblity(this.age)
     }
 }
 
@@ -30,9 +46,8 @@ let s2 = new Student('Ram',22,23456,70);
 let s3 = new Student('Sita',24,34567,80);
 let s4 = new Student('Valmiki',35,45678,20);
 let s5 = new Student('Lakshman',19,56789,40);
-s1.elig()
+s1.elig();
 s2.elig()
 s3.elig()
 s4.elig()
 s5.elig()
-Student.studentCount()
